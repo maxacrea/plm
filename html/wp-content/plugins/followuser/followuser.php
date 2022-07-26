12d71670759d92ae3d8b97babcdc0646b2debb5c
Andry <a.razanadrakoto@etechconsulting-mg.com>
2022-07-26 20:16:53 +0300

    my first commit

---
+++ html/wp-content/plugins/followuser/followuser.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     FollowUser
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     followuser
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Followuser
+ */
+
+// Your code starts here.
